f86c02e8bfdfd834d1822cdb9ab3be51fdc59090
Amoorer75 <Amoorer127@gmail.com>
2021-09-19 14:39:38 -0400

    completed js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function madlibs(){  
     let player = prompt('Would you like Story One or Story Two');
-    
+            
 
     if (player == 1 || player == 'one'
         || player == 'One' ){
@@ -20,7 +20,7 @@ function madlibs(){
             let story1 ='A vacation is when you take a trip to some ' + q1 + ' place near an amazing ' + q2 + '. A good vacation place is one where you can ride ' + q3 + ' or play ' + q4 + ' or go hunting for ' + q5 + '. I like to spend my time ' + q6 + ' or ' + q7 + '. When parents go on a vacation, they spend their time eating three ' + q8 + ' a day. Usually, fathers play ' + q9 + ', and mothers spend their time ' + q10 + '. Adults need vacations more than kids because adults are always very ' + q11 + ' because they have to work ' + q12 + ' hours every day all year just to afford their vacations!';
             
             document.getElementById('story1').outerHTML = story1;
-
+            
         }else 
         
         if (player == 2 || player == 'two'
@@ -40,7 +40,7 @@ function madlibs(){
                 let a13 = prompt("type a famous place")
                 let a14 = prompt("type an adjective")
                 
-                let story2 = "Today we took a {} fieldtrip to {}. They're famous for making {} and for cooking {} {}. Eating all that food made me feel {}. Next we enjoyed the local tradition of {} in the middle of the {}! Finally, we went shopping for souvenirs. I bought a {} {} for myself, and a {} {} for my favorite {}. I'll definitely never forget this {} trip!";
+                let story2 = "Today we took a" + a1 + " fieldtrip to " + a2 + ". They're famous for making " + a3 + " and for cooking " + a4 + " " + a5 + ". Eating all that food made me feel " + a6 + ". Next we enjoyed the local tradition of " + a7 + " in the middle of the " + a8 + "! Finally, we went shopping for souvenirs. I bought a " + a9 + " " + a10 + " for myself, and a " + a11 + " " + a12 + " for my favorite " + a13 + ". I'll definitely never forget this " + a14 + " trip!";
 
                 document.getElementById('story2').outerHTML = story2;
             }
@@ -51,5 +51,3 @@ function madlibs(){
 }
 document.getElementsByTagName("body").outerHTML = madlibs();
 
-
-console.log()
